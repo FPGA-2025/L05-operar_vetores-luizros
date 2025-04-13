f63d1e5ccae61c856e1e25c55ed06e56ccc6df53
Luiz <luizrodriguesesilva@outlook.com.br>
2025-04-13 14:11:40 -0300

    exercicio L05

--- operar_vetores.v
+++ operar_vetores.v
@@ -6,4 +6,9 @@ module operar_vetores(
     output [5:0] saida_not
 );
 	//insira seu código aqui
+
+    assign saida_or_bit_a_bit = a | b; // bit a bit
+    assign saida_or_logico = a || b; // Lógico
+    assign saida_not = {~b, ~a};
+
 endmodule
